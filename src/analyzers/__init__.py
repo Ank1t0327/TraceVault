6df7ddd7bd6d55f0ececc5c64a9c40037b05cadc
Ank1t0327 <ank1t032718@gmail.com>
2026-08-28 02:19:28 +0530

    Export CorrelationEngine and CorrelationNode in src/analyzers package

--- src/analyzers/__init__.py
+++ src/analyzers/__init__.py
@@ -1,5 +1,16 @@
 from .fs_analyzer import FileSystemAnalyzer
 from .timeline_engine import TimelineEngine, TimelineEvent, determine_severity
 from .ioc_detector import IOCDetector, IOC
+from .correlation_engine import CorrelationEngine, CorrelationNode, calculate_risk_score
 
-__all__ = ["FileSystemAnalyzer", "TimelineEngine", "TimelineEvent", "determine_severity", "IOCDetector", "IOC"]
+__all__ = [
+    "FileSystemAnalyzer",
+    "TimelineEngine",
+    "TimelineEvent",
+    "determine_severity",
+    "IOCDetector",
+    "IOC",
+    "CorrelationEngine",
+    "CorrelationNode",
+    "calculate_risk_score"
+]
