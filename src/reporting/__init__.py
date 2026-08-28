7e942eb49a5ebbb187d18d23fc010891da5c01df
Ank1t0327 <ank1t032718@gmail.com>
2026-08-28 23:27:34 +0530

    Fix package import exports in src/reporting

--- src/reporting/__init__.py
+++ src/reporting/__init__.py
@@ -1,4 +1,3 @@
-from .evidence_record import EvidenceRecord
 from .report_generator import ReportGenerator, ReportData
 
-__all__ = ["EvidenceRecord", "ReportGenerator", "ReportData"]
+__all__ = ["ReportGenerator", "ReportData"]
